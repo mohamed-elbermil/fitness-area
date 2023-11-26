be25df44ffc2d0998917abdd4508da96c889d6df
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2023-11-26 17:29:30 +0100

    version de novembre 2023

---
+++ common/menu.php
@@ -0,0 +1,26 @@
+
+<nav>
+
+    <a href="../index.php"> <img src="./img/Logo-blanc.png" alt="Logo" id="logo"></a>
+
+    <ul>
+        <li><a href="tarifs.php">Tarifs</a>
+            <a href="equipement.php">Equipement</a>
+            <a href="A-Propos.php">
+                <NOBR>à propos</NOBR>
+            </a>
+            <a href="sinscrire.php"><span>
+                    <NOBR>je m'inscris</NOBR>
+                </span></a>
+        </li>
+    </ul>
+
+
+
+
+    <div class="toggle">
+        <i class="fa-solid fa-bars ouvrir"></i>
+        <i class="fa-solid fa-xmark fermer"></i>
+    </div> 
+	
+</nav>
