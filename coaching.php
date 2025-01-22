92c2f03df46919b4405e80037783df702b8cd0f5
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-01-22 14:49:05 +0100

    Création de la page coach

--- coaching.php
+++ coaching.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
 <html lang="fr">
-
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fitness-Area - Coaching</title>
+    <title>Fitness-Area - Vous accueil du Lundi au Vendredi</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,120 +15,140 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Fav icon -->
     <link rel="icon" href="./img/favicon.ico">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./css/Style.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/variables.css">
 </head>
-<div id="barre" style="width: 96.9574px;"></div>
 
-<body id="body" class="dark-mode">
+<body>
+
+    <div id="barre" style="width: 984.211px;"></div>
+
     <header class="nav_header scroll">
         <?php
         include('./common/menu.php')
         ?>
     </header>
-    <nav>
-        <a href="./index.html"> <img src="./img/Logo-blanc.png" alt="Logo" id="logo"></a>
-        <ul>
-            <li><a href="tarifs.html">Tarifs</a>
-                <a href="equipement.html">Equipement</a>
-                <a href="A-Propos.html">
-                    <NOBR>à propos</NOBR>
-                </a>
-                <a href="sinscrire.html"><span>
-                        <NOBR>Contact</NOBR>
-                    </span></a>
-            </li>
-        </ul>
-        <div class="toggle">
-            <i class="fa-solid fa-bars ouvrir"></i>
-            <i class="fa-solid fa-xmark fermer"></i>
-        </div>
-    </nav>
 
 
-    </header>
     <main>
-        <img src="./img/banniere_materiel.png" alt="bannière équipement" id="banniere_material">
-        <div class="intro_equipement">
-            <h1 id="machine">Nos machines n'attendent que vous !</h1>
-            <p>Chez Fitness-Area vous êtes comme chez vous !<br> Vous souhaitez développer vos bras, la La poulie de traction est là pour ça !<br>Besoin de souffler sans trop souffrir, le tapis de course va vous suffire !<br>
-                Aucune excuse pour ne pas y’aller ! </p>
+        <div class="banner banner-form">
+            <div class="text_btn">
+                <span class="heading-label">Abonnements</span>
+                <h1>Découvrez nos formules<br>d’abonnement !
+                </h1>
+                <p>Des formules <strong>tout inclus et sans engagement</strong> pour accéder au sport premium et à des services incomparables.</p>
+
+            </div>
         </div>
 
-        <div class="machine">
-            <img src="./img/Rameur.webp" alt="rameur" id="material">
+        <!-- Team -->
+        <section class="team-band">
+            <h1>VOTRE ÉQUIPE L'Appart de fitness-area</h1>
+            <div class="team-bloc-band">
+                <div class="card-element">
+                    <div class="card-img">
+                        <img src="./img/coach/coach-1.png" alt="coach fitness-area">
+                    </div>
+                    <div class="element-title-img">
+                        <p>Julien</p>
+                        <span>Coach de musculation</span>
+                        <div class="social-media-coach">
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-instagram"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
 
-            <div class="titreNtexte">
-                <div class="titre_machine">
-                    <h1>Le rameur</h1>
-                    <span>Fitness-Area</span>
+
+                        </div>
+
+                    </div>
+
                 </div>
-                <p>Un rameur est une machine cardio d'intérieur qui imite les mouvements que vous pratiquez en ramant.
-                    Vous ne pouvez pas manquer cet appareil. Vous pouvez le trouver dans presque toutes les salles de fitness et de musculation, ainsi que la notre. Et pour quelques centaines d'euros, vous pouvez vous entraîner à la maison, vous pouvez donc profiter des multiples avantages de cet appareil de fitness.<br />En utilisant un rameur, vous pouvez travailler tous vos muscles. Vos bras tirent, vos jambes poussent, vos abdominaux assurent l’équilibre et votre dos rythme la cadence. Bref, il n'y a pas de place pour les paresseux, tout le monde est concerné. Pour connaître la bonne execution du mouvement n'hésitez pas à regarder nos tutoriels sur notre page <a href="https://www.facebook.com/Fitness-Area-104959968986758/?ref=pages_you_manage" target="_blank">Facebook</a> Cet exercice multi-articulaire permet de brûler un maximum de calories, surtout lorsqu'il est effectué à intensité minimale. Côté inconvénient, l’apprentissage d’une technique correcte semble être le principal frein pour les utilisateurs novices. Si vous souhaitez bénéficier de tous les bienfaits que peut vous apporter le rameur, et surtout éviter de vous blesser, prenez le temps d’acquérir une technique optimale ! Nous voyons encore trop de pratiquants se faire mal au dos en utilisant mal cet appareil.<br />
+                <div class="card-element">
+                    <div class="card-img">
+                        <img src="./img/coach/coach-1.png" alt="coach fitness-area">
+                    </div>
+                    <div class="element-title-img">
+                        <p>Julien</p>
+                        <span>Coach de musculation</span>
+                        <div class="social-media-coach">
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-instagram"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="card-element">
+                    <div class="card-img">
+                        <img src="./img/coach/coach-1.png" alt="coach fitness-area">
+                    </div>
+                    <div class="element-title-img">
+                        <p>Julien</p>
+                        <span>Coach de musculation</span>
+                        <div class="social-media-coach">
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-instagram"></i>
+                            </div>
+                            <div class="round-icon-border">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
             </div>
 
 
 
-            </p>
-        </div>
+        </section>
 
-        <div class="machine2">
-            <img src="./img/pouli.webp" alt="pouli" id="material"> <img src="./img/muscle-presta.png" alt="" id="photo_paysage">
+        <!-- TRIO BLOC -->
+        <section id="cest-partie" class="bloc-element bg-bloc">
+            <?php
+            include("./common/trio-bloc.php")
+            ?>
+        </section>
 
-            <div class="titreNtexte">
-                <h2>La poulie</h2>
-                <span>Fitness-Area</span>
-                <p>Une poulie est un dispositif de charge inductive très polyvalent. Son grand avantage est qu'il est possible de travailler en toute sécurité, mais aussi de maintenir une tension continue dans l'exécution des mouvements. En effet, lors des trois phases de contraction (concentrique, isométrique et excentrique) les muscles sont fortement sollicités. Le choix d'un appareil de musculation à poulie dépend des exercices que vous souhaitez effectuer. Découvrez si vous pouvez déplacer le nombre de poulies vers le haut, vers le bas ou simplement les poulies. Par exemple, le SlimBeam n'est pas seulement ultra-compact et peu encombrant, il dispose également de poulies réglables. Vous pouvez donc utiliser la poulie vers le bas, vers le haut ou au milieu.
-
-                    Idéalement, combinez une station de poulies avec un banc de musculation pour augmenter le nombre d'exercices. Vous pouvez faire à peu près n'importe quoi et entraîner à peu près n'importe quel muscle sur la machine.</p>
-            </div>
-
-        </div>
-
-        <div class="machine">
-            <img src="./img/Presse.webp" alt="presse" id="material"> <img src="./img/muscle-presta.png" alt="" id="photo_paysage">
-
-            <div class="titreNtexte">
-                <h3>La presse</h3>
-                <span>Fitness-Area</span>
-
-                <p>
-                    La presse est une machine de musculation qui fait travailler vos cuisses. Convient aussi bien aux pratiquants confirmés qu'aux débutants pour un entraînement intensif des membres inférieurs grâce à des mouvements accessibles et sûrs. Néanmoins, il existe quelques recommandations pour maximiser les avantages des exercices de presse. La presse à jambes travaille principalement les quadriceps (droit fémoral, vaste intermédiaire, latéral et médial) et une machine de musculation qui cible le grand fessier, les ischio-jambiers et les adducteurs. Cependant, cette invite varie légèrement en fonction de la position des béquilles sur la plate-forme. Placez vos pieds sur la planche pour souligner vos ischio-jambiers et vos fessiers. Enfin, écartez vos pieds avec vos orteils pointant vers l'extérieur pour resserrer davantage vos muscles adducteurs. De plus, l'inclinaison du dossier est réglable sur la plupart des bancs inclinés. N'ayez pas peur de changer de position pour travailler davantage les quadriceps et les ischio-jambiers.Enfin, les presses à jambes vous permettent de varier facilement votre tempo d'exécution avec des notions de temps tendues. Par exemple, la plupart des bodybuilders font légalement 3 à 5 séries de 8 à 12 répétitions avec 1 minute 30 de repos pour atteindre leurs objectifs d'hypertrophie. Mais combien de temps les muscles sont-ils tendus car faire 10 répétitions en 15 secondes ne donne pas le même résultat que faire 10 répétitions en 50 secondes.</p>
-            </div>
-
-        </div>
-
-        <div class="machine2">
-            <img src="./img/bike.webp" alt="vélo d'appartement" id="material"> <img src="./img/muscle-presta.png" alt="" id="photo_paysage">
-
-            <div class="titreNtexte">
-                <h4>Vélo éliptique</h4>
-                <span>Fitness-Area</span>
-                <p>Tout comme les rameurs, utiliser un vélo stationnaire pour perdre du poids est une très bonne entreprise. En fait, c'est une machine cardio pour brûler efficacement des calories et tonifier votre corps. Cependant, le vélo lui-même n'est pas une solution miracle. Par conséquent, la perte de poids est une question d'effort. Pour cela, il faut associer la régularité à une alimentation saine et équilibrée. En fonction de votre niveau et des objectifs de perte de poids que vous vous fixez, vous pourrez choisir entre le format cardio classique ou HIIT très prisé des adeptes du fitness. entraînement cardiaque
-                    Pédalez à vitesse modérée et augmentez progressivement la force. Cela encourage le corps à utiliser les réserves de graisse pour générer de l'énergie. À un rythme modéré, vous brûlerez 413 calories en une heure. À des intensités plus élevées, vous pouvez perdre entre 620 et 738 kcal. Faire de l'exercice régulièrement sur un vélo d'exercice peut entraîner une perte de poids importante en relativement peu de temps. Il faut savoir qu'1 gramme de graisse équivaut à 9 kilocalories (kcal). De plus, pour perdre 1 kg de graisse, il faut consommer 9 000 kcal.</p>
-            </div>
-
-        </div>
-
-
-
-
-
-        <!-- Avis et commentaires -->
-        <?php
-        include_once('./common/avis.php')
-        ?>
-
-
-
-
-
-
-
+        <!-- SERVICES INCLUS -->
+        <section class="services-inclus">
+            <?php
+            include('./common/our-values.php')
+            ?>
+        </section>
+        <section id="contact-us">
+            <?php
+            include('./common/contact-us.php')
+            ?>
+        </section>
     </main>
     <footer>
         <?php
@@ -139,15 +156,15 @@
         ?>
     </footer>
 
-    </div>
 
 
 
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+
+    <script src="/js/index.js"></script>
 
 </body>
 
