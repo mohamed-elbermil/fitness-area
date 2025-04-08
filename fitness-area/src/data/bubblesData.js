8ae1aa65d28ee5b50e60072efdbfdb53d02d2ea1
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-04-08 10:48:11 +0200

    ajout des label des liens bubbles

--- fitness-area/src/data/bubblesData.js
+++ fitness-area/src/data/bubblesData.js
@@ -1,17 +1,38 @@
 const bubbles = [
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-    {label: 'Lorem ipsum dolrokejzzeknezfizjhgiozejfik', href:'https://soblim.com/'},
-]
+  { label: "5 erreurs à éviter en musculation", href: "https://soblim.com/" },
+  { label: "Cardio ou muscu : que choisir ?", href: "https://soblim.com/" },
+  { label: "Comment rester motivé toute l’année", href: "https://soblim.com/" },
+  {
+    label: "Le top 7 des exercices brûle-graisse",
+    href: "https://soblim.com/",
+  },
+  {
+    label: "S’entraîner intelligemment, pas plus fort",
+    href: "https://soblim.com/",
+  },
+  {
+    label: "Routine express pour les jours chargés",
+    href: "https://soblim.com/",
+  },
+  { label: "Alimentation et sport : les bases", href: "https://soblim.com/" },
+  { label: "Pourquoi l’échauffement est crucial", href: "https://soblim.com/" },
+  {
+    label: "Gainage : 3 minutes par jour, ça change tout",
+    href: "https://soblim.com/",
+  },
+  { label: "Comment progresser sans se blesser", href: "https://soblim.com/" },
+  {
+    label: "Le sommeil, l’arme secrète des sportifs",
+    href: "https://soblim.com/",
+  },
+  {
+    label: "Hydratation : quand et combien boire ?",
+    href: "https://soblim.com/",
+  },
+  {
+    label: "Stretching : l’oublié qui fait la différence",
+    href: "https://soblim.com/",
+  },
+];
 
-export default bubbles
+export default bubbles;
