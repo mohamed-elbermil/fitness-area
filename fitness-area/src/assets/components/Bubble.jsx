2a20df387551cff654e690a239cda03e5047e1f0
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-06-13 12:24:45 +0200

    MAJ

--- fitness-area/src/assets/components/Bubble.jsx
+++ fitness-area/src/assets/components/Bubble.jsx
@@ -1,33 +1,34 @@
-import React, {useState, useEffect} from "react";
-import '../style/Bubble.css'
+import React, { useState, useEffect } from "react";
+import "../style/Bubble.css";
 import bubbles from "../../data/bubblesData";
 
 function Bubble() {
-    const [activeIndex, setActiveIndex] = useState(null);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        const random = Math.floor(Math.random() * 10); // 6 bulles
-        setActiveIndex(random);
-      }, 2000); // Change toutes les secondes
-  
-      return () => clearInterval(interval); // Nettoyage du setInterval
-    }, []);
-  
-    return (
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const random = Math.floor(Math.random() * 10); // 6 bulles
+      setActiveIndex(random);
+    }, 2000); // Change toutes les secondes
+
+    return () => clearInterval(interval); // Nettoyage du setInterval
+  }, []);
+
+  return (
+    <section className="center">
       <div className="container">
-        {bubbles.map((item,i) => (
+        {bubbles.map((item, i) => (
           <a
             key={i}
             href={item.href}
-            className={`bubble ${activeIndex === i ? 'active' : ''}`}
+            className={`bubble ${activeIndex === i ? "active" : ""}`}
           >
             {item.label}
           </a>
         ))}
       </div>
-    );
-  }
+    </section>
+  );
+}
 
-
-export default Bubble
+export default Bubble;
