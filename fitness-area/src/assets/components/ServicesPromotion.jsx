d107bebc34b6ff797e9710c4be9ad4442e8a7181
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-06-13 12:43:28 +0200

    MAJ

--- fitness-area/src/assets/components/ServicesPromotion.jsx
+++ fitness-area/src/assets/components/ServicesPromotion.jsx
@@ -6,41 +6,40 @@ function ServicesPromotion() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        <h3>Test</h3>
+        <h3>La musculation : force et bien-être</h3>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
-          sequi praesentium, quam dolor accusamus quia obcaecati voluptatum,
-          facilis dolore ipsa tenetur culpa quos! Excepturi, non.
+          La musculation est une discipline qui consiste à développer la force
+          et la masse musculaire grâce à des exercices ciblés. Elle améliore non
+          seulement l’apparence physique, mais aussi la santé globale, en
+          renforçant les os et en stimulant le métabolisme. Que ce soit avec des
+          poids libres, des machines ou le poids du corps, la régularité et la
+          bonne technique sont essentielles pour progresser en toute sécurité.
         </p>
       </div>
       <div className={styles.all_bloc}>
         <div className={styles.bloc}>
-          <i class="fa-solid fa-star"></i>
-          <span>Performance</span>
+          <i class="fa-solid fa-chart-simple"></i> <span>Performance</span>
           <p>
             Atteignez vos objectifs grâce à un encadrement personnalisé et un
             matériel de pointe, idéal pour progresser rapidement.
           </p>
         </div>
         <div className={styles.bloc}>
-          <i class="fa-solid fa-star"></i>
-          <span>Énergie</span>
+          <i class="fa-solid fa-power-off"></i> <span>Énergie</span>
           <p>
             Boostez votre vitalité avec nos cours collectifs variés et
             motivants, adaptés à tous les niveaux.
           </p>
         </div>
         <div className={styles.bloc}>
-          <i class="fa-solid fa-star"></i>
-          <span>Bien-être</span>
+          <i class="fa-solid fa-heart"></i> <span>Bien-être</span>
           <p>
             Détendez-vous et améliorez votre souplesse avec nos séances de yoga
             et stretching, dans un cadre convivial.
           </p>
         </div>
         <div className={styles.bloc}>
-          <i class="fa-solid fa-star"></i>
-          <span>Force</span>
+          <i class="fa-solid fa-hand-fist"></i> <span>Force</span>
           <p>
             Développez votre musculature avec notre équipement haut de gamme et
             nos programmes spécifiques.
