1a5bca2c53a379779b7dd484a118715199d1870f
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-06-23 14:03:57 +0200

    config du backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion MongoDB
+mongoose.connect('mongodb+srv://mohameddu69780:wS3OA9xBDWNZgFZB@fitness-area.ta0ul63.mongodb.net/?retryWrites=true&w=majority&appName=fitness-area', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Routes
+const contactRoutes = require('./routes/contact');
+app.use('/api/contact', contactRoutes);
+
+// Démarrage du serveur
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Serveur en écoute sur http://localhost:${PORT}`);
+});
